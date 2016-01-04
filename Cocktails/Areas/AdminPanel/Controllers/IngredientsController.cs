c8d622f06e1dd0efd26165d22cccd9a995135bf4
ReDERO <iredero@gmail.com>
2016-01-05 01:20:59 +0200

    Добавил админ. область. Добавил CRUD- и REST-шаблон для Ingredients

---
+++ Cocktails/Areas/AdminPanel/Controllers/IngredientsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CocktailsDatabase;
+
+namespace Cocktails.Areas.AdminPanel.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private CocktailsDbContext db = new CocktailsDbContext();
+
+        // GET: AdminPanel/Ingredients
+        public ActionResult Index()
+        {
+            return View(db.Ingredients.ToList());
+        }
+
+        // GET: AdminPanel/Ingredients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // GET: AdminPanel/Ingredients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminPanel/Ingredients/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingredients.Add(ingredient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ingredient);
+        }
+
+        // GET: AdminPanel/Ingredients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: AdminPanel/Ingredients/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ingredient);
+        }
+
+        // GET: AdminPanel/Ingredients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: AdminPanel/Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingredient ingredient = db.Ingredients.Find(id);
+            db.Ingredients.Remove(ingredient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
